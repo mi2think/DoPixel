a75497873e2bba9f1a00d09d6287e80cda18181c
mi2think <mi2think@gmail.com>
2015-10-11 02:44:34 +0800

    opengl step by step - diffuse lighting
    opengl step by step - diffuse lighting

---
+++ DpOpenGLDemo/18_DiffuseLighting/lighting_technique.h
@@ -0,0 +1,134 @@
+#pragma once
+
+#include "Pipeline.h"
+
+const char* vs = "#version 330\n"
+"layout(location = 0) in vec3 Position;\n"
+"layout(location = 1) in vec2 TexCoord;\n"
+"layout(location = 2) in vec3 Normal;\n"
+"uniform mat4 gWVP;"
+"uniform mat4 gWorld;"
+"out vec2 TexCoord0;"
+"out vec3 Normal0;"
+"void main()"
+"{"
+"gl_Position = gWVP * vec4(Position, 1.0);"
+"TexCoord0 = TexCoord;"
+"Normal0 = (gWorld * vec4(Normal, 0.0)).xyz;"
+"}";
+
+const char* ps = "#version 330\n"
+"in vec2 TexCoord0;"
+"in vec3 Normal0;"
+"struct DirectionalLight { vec3 color; float ambientIntensity; vec3 direction; float diffuseIntensity; };"
+"uniform DirectionalLight gDirectionalLight;"
+"uniform sampler2D gSampler;"
+"void main()"
+"{"
+"vec4 ambientColor = vec4(gDirectionalLight.color, 1.0f) * gDirectionalLight.ambientIntensity;"
+"float k = dot(normalize(Normal0), -gDirectionalLight.direction);"
+"vec4 diffuseColor;"
+"if (k > 0) { diffuseColor = vec4(gDirectionalLight.color, 1.0f) * gDirectionalLight.diffuseIntensity * k; }"
+"else { diffuseColor = vec4(0,0,0,0); }"
+"gl_FragColor = texture2D(gSampler, TexCoord0.xy) * (ambientColor + diffuseColor);"
+"}";
+
+struct DirectionalLight
+{
+	Vector3f color;
+	float ambientIntensity;
+	Vector3f direction;
+	float diffuseIntensity;
+};
+
+class LightingTechnique : public Technique
+{
+public:
+	LightingTechnique();
+
+	virtual bool Init();
+
+	void SetWVP(const Matrix44f& WVP);
+	void SetWorldMatrix(const Matrix44f& worldMatrix);
+	void SetTextureUnit(unsigned int textureUnit);
+	void SetDirectionalLight(const DirectionalLight& light);
+private:
+	GLuint WVPLocation_;
+	GLuint worldMatrixLocation_;
+	GLuint samplerLocation_;
+
+	struct
+	{
+		GLuint color;
+		GLuint ambientIntensity;
+		GLuint direction;
+		GLuint diffuseIntensity;
+		bool CheckValid() const
+		{
+			const unsigned int unvalidVal = 0xffffffff;
+			return color != unvalidVal && ambientIntensity != unvalidVal && direction != unvalidVal && diffuseIntensity != unvalidVal;
+		}
+	} dirLightLocation_;
+};
+
+LightingTechnique::LightingTechnique()
+	: WVPLocation_(0)
+	, worldMatrixLocation_(0)
+	, samplerLocation_(0)
+{
+}
+
+bool LightingTechnique::Init()
+{
+	if (!Technique::Init())
+		return false;
+
+	if (!AddShader(GL_VERTEX_SHADER, vs))
+		return false;
+
+	if (!AddShader(GL_FRAGMENT_SHADER, ps))
+		return false;
+
+	if (!Finalize())
+		return false;
+
+	WVPLocation_ = GetUniformLocation("gWVP");
+	worldMatrixLocation_ = GetUniformLocation("gWorld");
+	samplerLocation_ = GetUniformLocation("gSampler");
+
+	dirLightLocation_.color = GetUniformLocation("gDirectionalLight.color");
+	dirLightLocation_.ambientIntensity = GetUniformLocation("gDirectionalLight.ambientIntensity");
+	dirLightLocation_.direction = GetUniformLocation("gDirectionalLight.direction");
+	dirLightLocation_.diffuseIntensity = GetUniformLocation("gDirectionalLight.diffuseIntensity");
+
+	if (WVPLocation_ == 0xffffffff ||
+		worldMatrixLocation_ == 0xffffffff ||
+		samplerLocation_ == 0xffffffff ||
+		!dirLightLocation_.CheckValid())
+		return false;
+
+	return true;
+}
+
+void LightingTechnique::SetWorldMatrix(const Matrix44f& worldMatrix)
+{
+	glUniformMatrix4fv(worldMatrixLocation_, 1, GL_TRUE, &(worldMatrix.m[0][0]));
+}
+
+void LightingTechnique::SetWVP(const Matrix44f& WVP)
+{
+	glUniformMatrix4fv(WVPLocation_, 1, GL_TRUE, &(WVP.m[0][0]));
+}
+
+void LightingTechnique::SetTextureUnit(unsigned int textureUnit)
+{
+	glUniform1i(samplerLocation_, textureUnit);
+}
+
+void LightingTechnique::SetDirectionalLight(const DirectionalLight& light)
+{
+	glUniform3f(dirLightLocation_.color, light.color.x, light.color.y, light.color.z);
+	glUniform1f(dirLightLocation_.ambientIntensity, light.ambientIntensity);
+	glUniform3f(dirLightLocation_.direction, light.direction.x, light.direction.y, light.direction.z);
+	glUniform1f(dirLightLocation_.diffuseIntensity, light.diffuseIntensity);
+}
---
+++ DpOpenGLDemo/18_DiffuseLighting/main.cpp
@@ -0,0 +1,286 @@
+#include <stdio.h>
+#include <assert.h>
+
+#include <vector>
+#include <map>
+
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+
+#include "Pipeline.h"
+
+#include "DpVector2.h"
+#include "DpVector3.h"
+#include "DpMatrix44.h"
+using namespace dopixel::math;
+
+#include "lighting_technique.h"
+
+#pragma comment(lib, "glew32.lib")
+
+#define WINDOW_WIDTH	1024
+#define WINDOW_HEIGHT	768
+
+struct Vertex
+{
+	Vector3f pos;
+	Vector2f tex;
+	Vector3f normal;
+
+	Vertex(const Vector3f& _pos, const Vector2f& _tex)
+		: pos(_pos)
+		, tex(_tex)
+	{
+		normal.Zero();
+	}
+};
+
+class DiffuseLighting : public ICallbacks, public App
+{
+public:
+	DiffuseLighting();
+	~DiffuseLighting();
+
+	bool Init();
+
+	void Run();
+
+	virtual void RenderSceneCB();
+
+	virtual void KeyboardCB(int key);
+
+	virtual void PassiveMouseCB(int x, int y);
+private:
+	void CreateVertexBuffer();
+	void CalcNormals(Vertex* pVertices, unsigned int vertexCount);
+
+	PersProjInfo perProjInfo_;
+	Camera* camera_;
+	Texture* texture_;
+	float scale_;
+
+	DirectionalLight directionalLight_;
+	LightingTechnique* effect_;
+
+	GLuint VBO_;
+};
+
+DiffuseLighting::DiffuseLighting()
+	: texture_(nullptr)
+	, camera_(nullptr)
+	, effect_(nullptr)
+	, scale_(0.0f)
+{
+	directionalLight_.color = Vector3f(1.0f, 1.0f, 1.0f);
+	directionalLight_.ambientIntensity = 0.01f;
+	directionalLight_.diffuseIntensity = 0.75f;
+	directionalLight_.direction = Vector3f(1.0f, 0.0f, 0.0f);
+
+	perProjInfo_.FOV = 60.0f;
+	perProjInfo_.Width = WINDOW_WIDTH;
+	perProjInfo_.Height = WINDOW_HEIGHT;
+	perProjInfo_.zNear = 1.0f;
+	perProjInfo_.zFar = 100.0f;
+}
+
+DiffuseLighting::~DiffuseLighting()
+{
+	delete texture_;
+	delete camera_;
+	delete effect_;
+}
+
+bool DiffuseLighting::Init()
+{
+	camera_ = new Camera(Vector3f(0.0f, 0.0f, -3.0f), Vector3f(0.0f, 0.0f, 1.0f), Vector3f(0.0f, 1.0f, 0.0f), WINDOW_WIDTH, WINDOW_HEIGHT);
+	
+	CreateVertexBuffer();
+
+	texture_ = new Texture(GL_TEXTURE_2D, "checkerboard.png");
+	if (!texture_->Load())
+		return false;
+
+	effect_ = new LightingTechnique();
+	if (!effect_->Init())
+		return false;
+	effect_->Enable();
+	effect_->SetTextureUnit(0);
+
+	return true;
+}
+
+void DiffuseLighting::CreateVertexBuffer()
+{
+	const int vertexCount = 12;
+
+	Vertex vertices[vertexCount] = {
+		// 1
+		{ Vector3f(0.0f, 1.0f, 0.0f), Vector2f(0.5f, 1.0f) },
+		{ Vector3f(-1.0f, -1.0f, 0.5773f), Vector2f(1.0f, 0.0f) },
+		{ Vector3f(1.0f, -1.0f, 0.5773f), Vector2f(0.0f, 0.0f) },
+		// 2
+		{ Vector3f(0.0f, 1.0f, 0.0f), Vector2f(0.5f, 1.0f) },
+		{ Vector3f(0.0f, -1.0f, -1.15475f), Vector2f(1.0f, 0.0f) },
+		{ Vector3f(-1.0f, -1.0f, 0.5773f), Vector2f(0.0f, 0.0f) },
+		// 3
+		{ Vector3f(0.0f, 1.0f, 0.0f), Vector2f(0.5f, 1.0f) },
+		{ Vector3f(1.0f, -1.0f, 0.5773f), Vector2f(1.0f, 0.0f) },
+		{ Vector3f(0.0f, -1.0f, -1.15475f), Vector2f(0.0f, 0.0f) },
+		// 4
+		{ Vector3f(-1.0f, -1.0f, 0.5773f), Vector2f(0.5f, 1.0f) },
+		{ Vector3f(0.0f, -1.0f, -1.15475f), Vector2f(1.0f, 0.0f) },
+		{ Vector3f(1.0f, -1.0f, 0.5773f), Vector2f(0.0f, 0.0f) }
+	};
+
+	CalcNormals(vertices, vertexCount);
+
+	glGenBuffers(1, &VBO_);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO_);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+}
+
+void DiffuseLighting::CalcNormals(Vertex* pVertices, unsigned int vertexCount)
+{
+	// compute each triangle normal
+	for (unsigned int i = 0; i < vertexCount; i += 3)
+	{
+		auto& v0 = pVertices[i];
+		auto& v1 = pVertices[i + 1];
+		auto& v2 = pVertices[i + 2];
+
+		Vector3f u = v1.pos - v0.pos;
+		Vector3f v = v2.pos - v0.pos;
+		Vector3f n = CrossProduct(u, v);
+		n.Normalize();
+
+		v0.normal = n;
+		v1.normal = n;
+		v2.normal = n;
+	}
+
+	// compute vertex normal which have same position
+	// k: vertex
+	// v: normals
+	struct CompareVector3f
+	{
+		bool operator() (const Vector3f& lhs, const Vector3f& rhs) const
+		{
+			if (lhs.x != rhs.x)
+				return lhs.x < rhs.x;
+			if (lhs.y != rhs.y)
+				return lhs.y < rhs.y;
+			return lhs.z < rhs.z;
+		}
+	};
+
+	std::map<Vector3f, Vector3f, CompareVector3f> vertexNormals;
+	for (unsigned int i = 0; i < vertexCount; ++i)
+	{
+		auto& v = pVertices[i];
+		auto& normal = vertexNormals[v.pos];
+		normal += v.normal;
+	}
+
+	// normalize
+	for (auto& kv : vertexNormals)
+	{
+		kv.second.Normalize();
+	}
+
+	// assign to vertex
+	for (unsigned int i = 0; i < vertexCount; ++i)
+	{
+		auto& v = pVertices[i];
+		const auto& normal = vertexNormals[v.pos];
+		v.normal = normal;
+	}
+}
+
+void DiffuseLighting::Run()
+{
+	GLUTBackendRun(this);
+}
+
+void DiffuseLighting::RenderSceneCB()
+{
+	camera_->OnRender();
+
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	scale_ += 0.1f;
+
+	Pipeline p;
+	p.Rotate(0.0f, angle2radian(scale_), 0.0f);
+	p.WorldPos(0.0f, 0.0f, 1.0f);
+	p.SetCamera(*camera_);
+	p.SetPerspectiveProj(perProjInfo_);
+
+	effect_->SetWVP(p.GetOGLWorldViewProjTrans());
+	effect_->SetWorldMatrix(p.GetOGLWorldTrans());
+	effect_->SetDirectionalLight(directionalLight_);
+
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
+	glEnableVertexAttribArray(2);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO_);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const GLvoid*)12);
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const GLvoid*)20);
+
+	texture_->Bind(GL_TEXTURE0);
+	glDrawArrays(GL_TRIANGLES, 0, 12);
+
+	glDisableVertexAttribArray(0);
+	glDisableVertexAttribArray(1);
+	glDisableVertexAttribArray(2);
+
+	glutSwapBuffers();
+}
+
+void DiffuseLighting::KeyboardCB(int key)
+{
+	switch (key)
+	{
+	case 'q':
+		GLUTBackendLeaveMainLoop();
+		break;
+	case 'a':
+		directionalLight_.ambientIntensity += 0.05f;
+		break;
+	case 's':
+		directionalLight_.ambientIntensity -= 0.05f;
+		break;
+	case 'z':
+		directionalLight_.diffuseIntensity += 0.05f;
+		break;
+	case 'x':
+		directionalLight_.diffuseIntensity -= 0.05f;
+		break;
+	default:
+		break;
+	}
+}
+
+void DiffuseLighting::PassiveMouseCB(int x, int y)
+{
+	camera_->OnMouse(x, y);
+}
+
+int main(int argc, char** argv)
+{
+	GLUTBackendInit(argc, argv, false, false);
+
+	if (!GLUTBackendCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, false, "tutorial 18"))
+		return 1;
+
+	DiffuseLighting* app = new DiffuseLighting();
+	if (!app->Init())
+		return 1;
+
+	app->Run();
+
+	delete app;
+	return 0;
+}
