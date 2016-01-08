f959da8024e6b78745609b80a0055fbc961dc84d
Wenbiao Hou <mi2think@gmail.com>
2016-01-08 22:29:57 +0800

    add scope newline to log
    add scope newline to log

--- DpLib/DpLog.cpp
+++ DpLib/DpLog.cpp
@@ -19,34 +19,14 @@ namespace dopixel
 {
 	Log g_Log;
 
-	Log::Log() : hwnd_(nullptr)
+	Log::Log()
+		: hwnd_(nullptr)
+		, newline_(true)
 	{}
 
 	void Log::WriteBuf(int level, const char* fmt, ...)
 	{
-		switch (level)
-		{
-		case Info:
-			// info is bright white
-			SetTextColor(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
-			break;
-		case Warn:
-			// warn is bright yellow
-			SetTextColor(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-			break;
-		case Error:
-			// error is red
-			SetTextColor(FOREGROUND_RED | FOREGROUND_INTENSITY);
-			break;
-		case Fatal:
-			// fatal is white on red
-			SetTextColor(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY | BACKGROUND_RED | BACKGROUND_INTENSITY);
-			break;
-		default:
-			// other is cyan
-			SetTextColor(FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
-			break;
-		}
+		SetTextColor(GetTextColor(level));
 
 		static char buffer[1024];
 		va_list ap;
@@ -55,10 +35,43 @@ namespace dopixel
 		va_end(ap);
 		buffer[1023] = 0;
 
+		std::ostream* os = nullptr;
 		if (level == Error || level == Fatal)
-			std::cout << buffer << std::endl;
+			os = &std::cout;
 		else
-			std::cerr << buffer << std::endl;
+			os = &std::cerr;
+		(*os) << buffer;
+		if (newline_)
+			(*os) << std::endl;
+	}
+
+	unsigned short Log::GetTextColor(int level)
+	{
+		unsigned short color;
+		switch (level)
+		{
+		case Info:
+			// info is bright white
+			color = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY;
+			break;
+		case Warn:
+			// warn is bright yellow
+			color = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY;
+			break;
+		case Error:
+			// error is red
+			color = FOREGROUND_RED | FOREGROUND_INTENSITY;
+			break;
+		case Fatal:
+			// fatal is white on red
+			color = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY | BACKGROUND_RED | BACKGROUND_INTENSITY;
+			break;
+		default:
+			// other is cyan
+			color = FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY;
+			break;
+		}
+		return color;
 	}
 
 	void Log::SetTextColor(unsigned short color)
@@ -68,5 +81,16 @@ namespace dopixel
 			hwnd_ = GetStdHandle(STD_OUTPUT_HANDLE);
 		}
 		SetConsoleTextAttribute(hwnd_, color);
+	}
+
+	Log::ScopeNewline::ScopeNewline(bool newline)
+	{
+		newline_ = g_Log.GetNewline();
+		g_Log.SetNewline(newline);
+	}
+
+	Log::ScopeNewline::~ScopeNewline()
+	{
+		g_Log.SetNewline(newline_);
 	}
 }
--- DpLib/DpLog.h
+++ DpLib/DpLog.h
@@ -17,6 +17,13 @@ namespace dopixel
 	class Log
 	{
 	public:
+		struct ScopeNewline
+		{
+			ScopeNewline(bool newline);
+			~ScopeNewline();
+			bool newline_;
+		};
+
 		enum LogLevel
 		{
 			Info,
@@ -27,19 +34,26 @@ namespace dopixel
 		Log();
 
 		void WriteBuf(int level, const char* fmt, ...);
+		unsigned short GetTextColor(int level);
 		void SetTextColor(unsigned short color);
+		void SetNewline(bool newline) { newline_ = newline; }
+		bool GetNewline() const { return newline_; }
 	private:
 		void* hwnd_;
+		bool newline_;
 	};
 
 	extern Log g_Log;
 
 	#define LOG_TO(level, fmt, ...) g_Log.WriteBuf(level, fmt, __VA_ARGS__)
-
 	#define LOG_INFO(fmt, ...)	LOG_TO(Log::Info, fmt, __VA_ARGS__)
 	#define LOG_WARN(fmt, ...)	LOG_TO(Log::Warn, fmt, __VA_ARGS__)
 	#define LOG_ERROR(fmt, ...)	LOG_TO(Log::Error, fmt, __VA_ARGS__)
 	#define LOG_FATAL(fmt, ...)	LOG_TO(Log::Fatal, fmt, __VA_ARGS__)
+
+	#define LOGSCOPE_NAMELINE_CAT(name, line) name##line
+	#define LOGSCOPE_NAMELINE(name, line) LOGSCOPE_NAMELINE_CAT(name, line)
+	#define LOGSCOPE_NEWLINE(newline) Log::ScopeNewline LOGSCOPE_NAMELINE(LogScope, __LINE__)(newline)
 }
 
 #endif
